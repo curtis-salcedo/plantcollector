327d55cea050f9cc4d5175c7c6d1600a61e626d5
Curtis <curtissalcedo@gmail.com>
2023-04-21 10:40:45 -0600

    Finished part 2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+    name = models.CharField(max_length=25)
+    type = models.CharField(max_length=25)
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
